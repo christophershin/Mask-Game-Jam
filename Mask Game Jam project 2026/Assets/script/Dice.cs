78c7f9f97487e61a08b84f40822bc4855317b7b1
christophershin <dinojun323@gmail.com>
2026-01-31 14:55:53 -0500

    Dice roll

--- Mask Game Jam project 2026/Assets/script/Dice.cs
+++ Mask Game Jam project 2026/Assets/script/Dice.cs
@@ -1,7 +1,23 @@
+using System.Collections.Generic;
+using NUnit.Framework.Internal;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Dice : MonoBehaviour
 {
+
+
+    bool rolled = false;
+    bool grabbed = false;
+    Vector2 offset;
+    int diceNumber;
+
+    public int minDiceValue = 1;
+    public int maxDiceValue = 6;
+
+    public List<Sprite> DiceImage; 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +27,49 @@ public class Dice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePos = Input.mousePosition;
+
+
+        if(rolled)
+        {
+            diceNumber = Random.Range(minDiceValue, maxDiceValue + 1);
+            Debug.Log(diceNumber);
+            GetComponent<SpriteRenderer>().sprite = DiceImage[diceNumber-1];
+
+            rolled = false;
+        }
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if (rolled == false)
+            {
+                Debug.Log("grabbed");
+                grabbed = true;
+                transform.Rotate(new Vector3(0, 0, 35));
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            if (grabbed == true && rolled == false)
+            {
+                Debug.Log("not grabbed");
+                grabbed = false;
+                rolled = true;
+            }
+        }
+    }
+
+
+    private void OnMouse()
+    {
+
+    }
+
+    private void OnMouseUp()
+    {
+
+
     }
 }
