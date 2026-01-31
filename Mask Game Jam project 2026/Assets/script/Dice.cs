92b921535fdd12e924ec68e013b345cff997c9f7
CarterN55 <carterng55@gmail.com>
2026-01-31 15:45:15 -0500

    test

--- Mask Game Jam project 2026/Assets/script/Dice.cs
+++ Mask Game Jam project 2026/Assets/script/Dice.cs
@@ -29,7 +29,7 @@ public class Dice : MonoBehaviour
     {
         Vector3 mousePos = Input.mousePosition;
 
-        // Dice
+        // Dice roll
         if(rolled)
         {
             diceNumber = Random.Range(minDiceValue, maxDiceValue + 1);
